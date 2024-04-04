86722bd428d839d87709a01e20e319242cfa9202
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-04 10:51:34 -0700

    copmlete simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [randomDog, setRandomDog] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const dog = await response.json();
+      console.log(dog.message);
+      setRandomDog(dog.message);
+      setIsLoaded(true);
+    };
+    fetchDogImage();
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img src={randomDog} alt="A Random Dog" />;
+}
